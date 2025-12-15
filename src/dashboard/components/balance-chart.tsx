0b7fa5ba991cc8aa3395dc78b473e1a756f3c80f
André Luís Maldonado Daniel <andre_lmd2@hotmail.com>
2025-12-15 18:09:57 -0300

    refactor: changes balance chart o area

--- src/dashboard/components/balance-chart.tsx
+++ src/dashboard/components/balance-chart.tsx
@@ -1,8 +1,14 @@
 import { Dayjs } from "dayjs"
-import { useMemo } from "react"
-import { CartesianGrid, Line, LineChart, XAxis, YAxis } from "recharts"
+import { Area, AreaChart, CartesianGrid, XAxis } from "recharts"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../../components/ui/card"
-import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from "../../components/ui/chart"
+import {
+	ChartConfig,
+	ChartContainer,
+	ChartLegend,
+	ChartLegendContent,
+	ChartTooltip,
+	ChartTooltipContent,
+} from "../../components/ui/chart"
 import { Skeleton } from "../../components/ui/skeleton"
 import { IChartData } from "../../helpers/balance-chart-calculation"
 import { useDelayedLoading } from "../../hooks/use-delayed-loading"
@@ -10,11 +16,11 @@ import { useDelayedLoading } from "../../hooks/use-delayed-loading"
 const chartConfig = {
 	realizedBalance: {
 		label: "Realizado",
-		color: "var(--chart-1)",
+		color: "var(--foreground)",
 	},
 	projectedBalance: {
 		label: "Projetado",
-		color: "var(--chart-2)",
+		color: "var(--muted-foreground)",
 	},
 } satisfies ChartConfig
 
@@ -26,14 +32,10 @@ interface IBalanceChartProps {
 
 export const BalanceChart = ({ data, date, isLoading = false }: IBalanceChartProps) => {
 	const showDelayedSkeleton = useDelayedLoading(isLoading, 500)
-	const allValues = useMemo(() => data.flatMap((item) => [item.realizedBalance, item.projectedBalance]), [data])
 
 	if (showDelayedSkeleton) {
 		return <Skeleton className="flex-1 h-[368px] w-full" />
 	}
-
-	const maxBalance = allValues.length > 0 ? Math.max(...allValues) : 0
-	const minBalance = allValues.length > 0 ? Math.min(...allValues) : 0
 
 	return (
 		<Card className="flex-1">
@@ -41,35 +43,48 @@ export const BalanceChart = ({ data, date, isLoading = false }: IBalanceChartPro
 				<CardTitle>Balanço do mês</CardTitle>
 				<CardDescription>{date.format("MMMM YYYY")}</CardDescription>
 			</CardHeader>
-			<CardContent>
+			<CardContent className="p-8">
 				<ChartContainer config={chartConfig} className="aspect-auto h-[250px] w-full">
-					<LineChart
-						accessibilityLayer
-						data={data}
-						margin={{
-							left: 12,
-							right: 12,
-						}}
-					>
-						<CartesianGrid vertical={false} horizontal={true} />
-						<YAxis domain={[minBalance * -1.3, maxBalance * 1.3]} tick={false} axisLine={false} />
-						<XAxis dataKey={"date"} tickLine={false} axisLine={false} tickMargin={8} />
-						<ChartTooltip cursor={false} content={<ChartTooltipContent />} />
-						<Line
-							dataKey={"realizedBalance"}
-							type={"natural"}
-							stroke={"var(--color-realizedBalance)"}
-							strokeWidth={2}
-							dot={false}
+					<AreaChart data={data}>
+						<defs>
+							<linearGradient id={"fillRealizedBalance"} x1="0" y1="0" x2="0" y2="1">
+								<stop offset={"50%"} stopColor="var(--color-realizedBalance)" stopOpacity={0.8} />
+								<stop offset={"95%"} stopColor="var(--color-realizedBalance)" stopOpacity={0.1} />
+							</linearGradient>
+							<linearGradient id="fillProjectedBalance" x1="0" y1="0" x2="0" y2="1">
+								<stop offset="5%" stopColor="var(--color-projectedBalance)" stopOpacity={0.8} />
+								<stop offset="95%" stopColor="var(--color-projectedBalance)" stopOpacity={0.1} />
+							</linearGradient>
+						</defs>
+						<CartesianGrid vertical={false} />
+						<XAxis dataKey="date" tickLine={false} axisLine={false} tickMargin={8} minTickGap={32} />
+						<ChartTooltip
+							cursor={false}
+							content={
+								<ChartTooltipContent
+									labelFormatter={(value) => {
+										return value
+									}}
+									indicator="dot"
+								/>
+							}
 						/>
-						<Line
-							dataKey={"projectedBalance"}
-							type={"natural"}
-							stroke={"var(--color-projectedBalance)"}
-							strokeWidth={2}
-							dot={false}
+						<Area
+							dataKey="projectedBalance"
+							type="natural"
+							fill="url(#fillProjectedBalance)"
+							stroke="var(--color-projectedBalance)"
+							stackId="a"
 						/>
-					</LineChart>
+						<Area
+							dataKey="realizedBalance"
+							type="natural"
+							fill="url(#fillRealizedBalance)"
+							stroke="var(--color-realizedBalance)"
+							stackId="a"
+						/>
+						<ChartLegend content={<ChartLegendContent />} />
+					</AreaChart>
 				</ChartContainer>
 			</CardContent>
 		</Card>
